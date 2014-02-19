ae3facc815973510f9f83bd505099f5857201cee
Nils Gemeinhardt <git@nilsgemeinhardt.de>
2014-02-19 19:46:36 +0100

    add tour pages and overview

---
+++ _plugins/tour_overview_page.rb
@@ -0,0 +1,52 @@
+module Jekyll
+  class TourOverviewPage < Page
+    def initialize(site)
+      @site = site
+      @base = site.source
+      @dir  = '/'
+      @name = 'index.html'
+
+      process(@name)
+      self.data = page_data
+    end
+
+    private
+
+    def page_data
+      {
+        'layout' => 'tour_overview',
+        'tours'  => load_tours
+      }
+    end
+
+    def load_tours
+      Dir['_tours/*.yml'].map do |path|
+        tour_data(path)
+      end
+    end
+
+    def tour_data(file_path)
+      YAML.load(File.read(file_path)).tap do |tour|
+        tour['path'] = tour_path(file_path)
+      end
+    end
+
+    def tour_path(file_path)
+      File.basename(file_path, '.yml').gsub('_', '-')
+    end
+  end
+
+  class TourOverviewGenerator < Generator
+    safe true
+
+    def generate(site)
+      TourOverviewPage.new(site).tap do |page|
+        page.render(site.layouts, site.site_payload)
+        page.write(site.dest)
+
+        site.pages << page
+        site.static_files << page
+      end
+    end
+  end
+end
---
+++ _plugins/tour_page.rb
@@ -0,0 +1,46 @@
+module Jekyll
+  class TourPage < Page
+    def initialize(site, name)
+      @site = site
+      @base = site.source
+      @dir  = "/#{name.gsub('_', '-')}"
+      @name = 'index.html'
+
+      process(@name)
+      self.data = load_data(name)
+    end
+
+    private
+
+    def load_data(name)
+      load_yaml("_tours/#{name}.yml").tap do |data|
+        data['layout'] = 'tour'
+      end
+    end
+
+    def load_yaml(path)
+      YAML.load(File.read(File.join(@base, path)))
+    end
+  end
+
+  class TourGenerator < Generator
+    safe true
+
+    def generate(site)
+      Dir['_tours/*.yml'].each do |path|
+        name = File.basename(path, '.yml')
+        write_tour_page(site, path, name)
+      end
+    end
+
+    def write_tour_page(site, path, name)
+      TourPage.new(site, name).tap do |tour|
+        tour.render(site.layouts, site.site_payload)
+        tour.write(site.dest)
+
+        site.pages << tour
+        site.static_files << tour
+      end
+    end
+  end
+end
